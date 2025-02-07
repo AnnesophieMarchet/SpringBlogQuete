3e7eff95725a576aeeecf898f45f9c97e1945865
Anne-sophie Marchet <annesophiemarchet@gmail.com>
2025-02-07 15:12:34 +0100

    init projet spring

---
+++ src/main/java/com/example/springblog/springblog/SpringBlogQueteApplication.java
@@ -0,0 +1,13 @@
+package com.example.springblog.springblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBlogQueteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBlogQueteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springblog/springblog/SpringBlogQueteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springblog.springblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBlogQueteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
